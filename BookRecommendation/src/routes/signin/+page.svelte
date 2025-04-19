7326d7878fd5ca410a2e272b52c97ec3f5c5ddaf
XZD47P <kutizoli2003@gmail.com>
2025-04-19 17:43:43 +0200

    Added RegisterForm for Signin page

--- BookRecommendation/src/routes/signin/+page.svelte
+++ BookRecommendation/src/routes/signin/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import LoginForm from "$lib/components/LoginForm.svelte";
+    import RegisterForm from "$lib/components/RegisterForm.svelte";
 </script>
 
 <LoginForm/>
+<RegisterForm/>
